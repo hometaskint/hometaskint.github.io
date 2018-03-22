65a89083ac65b1e8ba2a5ab572558aa7da994f52
hometaskint <exultantman@yandex.ru>
2018-03-22 09:39:46 +0300

    Update form.py

--- form.py
+++ form.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+from http.server import HTTPServer, CGIHTTPRequestHandler
+server_address = ("", 8000)
+httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+httpd.serve_forever()
 import cgi
 import html
 
